d833b52ff455e55205e2b1f132fa96c425e7cf60
megahomyak <g.megahomyak@gmail.com>
2025-06-15 00:13:49 +0500

    Alright, this is fucked. I think it's gonna be better to go manual at first and automate from there

--- sizeCalculator.js
+++ sizeCalculator.js
@@ -1,31 +1,18 @@
-let line = (xd, yd, style) => ctx => ctx.sendLine({ x: 0, y: 0 }, { x: xd + ctx.x, y: yd + ctx.y }, style);
-let offset = (xd, yd, target) => ctx => target({ ...ctx, x: ctx.x + xd, y: ctx.y + yd });
+// High-level structure: "everything is a rectangle"
+// Low-level structure: "everything consists of lines"
 
-let combine = (...targets) => (x, y) => {
-    for (let target of targets) {
-        target(x, y);
-    }
+let line = (xd, yd) => (x, y) => ({
+    lines: [{ p1: { x, y }, p2: { x: x + xd, y: y + yd } }],
+    size: {
+        width: xd,
+        height: yd,
+    },
+});
+
+let boxHoleSide = (x, )
+
+let makeBox = boxSideSize => {
+    let tubeDiameter = boxSideSize / 2;
+    let tubeCutDiameter = tubeDiameter * 0.2;
 };
-
-let fold = (xd, yd) => line(xd, yd, "thin");
-let cut = (xd, yd) => line(xd, yd, "thick");
-
-let point = () => (x, y) => ({ x, y });
-let offset = (xd, yd, target) => (x, y, generator) => target(x + xd, y + yd);
-
-let struct = fieldProducer => (x, y) => fieldProducer(variant => variant(x, y));
-let line = (xd, yd) => struct(field => ({
-    beginning: field(point()),
-    end: field(offset(xd, yd, point())),
-}));
-let box = (xd, yd) => struct(field => ({
-    top: field(line(xd, 0)),
-    left: field(line(0, yd)),
-    bottom: field(offset(0, yd, line(xd, 0))),
-    right: field(offset(xd, 0, line(0, yd))),
-}));
-let papercraft = boxSideSize => struct(field => ({
-
-}));
-
-console.log(papercraft(4)(0, 0));
+console.log(makeBox(4));
