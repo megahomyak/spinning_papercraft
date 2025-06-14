d1105dc01a5f087a5358be4eec8690f9c3a4c766
megahomyak <g.megahomyak@gmail.com>
2025-06-14 21:06:24 +0500

    I should make a line my atom (because that's how cuts would be made) and add size querying to make stuff like bottom-joining possible

--- sizeCalculator.js
+++ sizeCalculator.js
@@ -1,7 +1,19 @@
-let makeUnion = variantProducer => (x, y) => variantProducer(variant => variant(x, y));
-let makePoint = (xd, yd) => (x, y) => ({ x: x + xd, y: y + yd });
-let makeLine = (xd, yd) => ;
-let makeBox = (w, h) => union(line(w, 0), line(0, h), offset(0, w, line(0, h)), offset(0, h, line(w, 0)));
-let boxSideSize = 4;
-let knobHoleSideSize = boxSideSize/2*0.8;
-let 
+let point = () => (x, y) => ({ x, y });
+let offset = (xd, yd, target) => (x, y, generator) => target(x + xd, y + yd);
+
+let struct = fieldProducer => (x, y) => fieldProducer(variant => variant(x, y));
+let line = (xd, yd) => struct(field => ({
+    beginning: field(point()),
+    end: field(offset(xd, yd, point())),
+}));
+let box = (xd, yd) => struct(field => ({
+    top: field(line(xd, 0)),
+    left: field(line(0, yd)),
+    bottom: field(offset(0, yd, line(xd, 0))),
+    right: field(offset(xd, 0, line(0, yd))),
+}));
+let papercraft = boxSideSize => struct(field => ({
+
+}));
+
+console.log(papercraft(4)(0, 0));
