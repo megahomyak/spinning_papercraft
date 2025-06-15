4f16804f77abf2540c8edab79637e91676d4945a
megahomyak <g.megahomyak@gmail.com>
2025-06-15 15:36:07 +0500

    I guess these calculations should be enough

---
+++ sizeCalculator.mjs
@@ -0,0 +1,42 @@
+let g = {};
+g.categories = {};
+let category = (categoryName, handler) => {
+    g.categories[categoryName] = {};
+    handler(new Proxy(g, {
+        set: (_target, name, value) => {
+            g[name] = value;
+            g.categories[categoryName][name] = value;
+            return true;
+        },
+    }));
+};
+
+category("params", g => {
+    g.boxSideSize = 4;
+    g.tubeFlatHeight = 4;
+    g.tubeOffsetWidth = 1;
+    g.tubeFlapCutLength = 1;
+    g.tubeSlitLength = (g.tubeFlatHeight / 2) - (g.tubeFlapCutLength * 2);
+});
+
+category("tube", g => {
+    g.tubeDiameter = g.boxSideSize / 2;
+    g.tubeHoleDiameter = g.tubeDiameter * 0.2;
+    g.tubeRadius = g.tubeDiameter / 2;
+    g.tubeHalfCircleLength = g.tubeRadius * Math.PI;
+    g.tubeFlatWidth = (g.tubeOffsetWidth + g.tubeHalfCircleLength) * 2 + g.tubeDiameter;
+    g.tubeFlatPerpendicularCutWidth = (g.tubeFlatWidth / 2) - g.tubeHoleDiameter;
+});
+
+category("box side with hole", g => {
+    g.tubeSquareOffset = (g.boxSideSize - g.tubeHoleDiameter) / 2;
+    g.tubeSquareSideSize = g.tubeHoleDiameter;
+    g.tubeSquareEnd = g.tubeOffsetWidth + g.tubeSquareSideSize;
+    g.shiftedSlantBiases = [
+        Math.tan(Math.PI / 6),
+        Math.tan(Math.PI / 12),
+    ];
+    g.equalSlantBias = Math.tan(Math.PI / 8);
+});
+
+console.log(g.categories);
